10e6a800b22e4018c10d6b02d6c54753231c008b
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-16 18:00:09 -0800

    With Animated enso circle and collapsing cards

--- src/index.js
+++ src/index.js
@@ -9,56 +9,61 @@ const getInspoBttn = document.querySelector("#get-inspo-btn");
 document.addEventListener("DOMContentLoaded", () => {
     const inspoCollection = document.getElementById("inspo-collection")
 
-    fetch(url)
-    .then(response => response.json())
-    .then(renderInspoCards);
-
-
-    function renderInspoCards(inspo){
-        inspo.forEach(function renderSingleCard(inspo) {
-            // console.log(inspo);
-
-            inspoCard = document.createElement("div")
-            inspoCard.className = "card";
-            // console.log(inspoCard);
-
-            inspoAuthorTag = document.createElement("h3")
-            authorName = document.createTextNode(inspo.author)
-            inspoAuthorTag.appendChild(authorName)
-            inspoCard.append(inspoAuthorTag)
-
-            inspoImgTag = document.createElement("img")
-            inspoImgTag.src = inspo.image
-            inspoImgTag.className = "inspo-avatar"
-            inspoCard.append(inspoImgTag)
-
-            inspoQuoteTag = document.createElement("h4")
-            inspoQuote = document.createTextNode(inspo.quote)
-            inspoQuoteTag.appendChild(inspoQuote)
-            inspoCard.append(inspoQuoteTag)
-
-            inspoLikesTag = document.createElement("p")
-            inspoLikesTag.innerText = `${inspo.likes}`
-            inspoCard.append(inspoLikesTag)
-
-
-
-            inspoCollection.append(inspoCard);
-            // console.log(inspoCollection);
-        });
-        
-    }
-
-
-
-
     getInspoBttn.addEventListener("click", () => {
         // hide & seek with the form 
         addInspo = !addInspo;
         if (addInspo) {
             inspoFormContainer.style.display = "block";
+            inspoCollection.style.display = "block";
         } else {
             inspoFormContainer.style.display = "none";
+            inspoCollection.style.display = "none";
+        }
+
+        fetch(url)
+        .then(response => response.json())
+        .then(renderInspoCards);
+
+        function renderInspoCards(inspo){
+            inspo.forEach(function renderSingleCard(inspo) {
+                // console.log(inspo);
+    
+                inspoCard = document.createElement("div")
+                inspoCard.className = "card";
+                // console.log(inspoCard);
+    
+                inspoAuthorTag = document.createElement("h3")
+                authorName = document.createTextNode(inspo.author)
+                inspoAuthorTag.appendChild(authorName)
+                inspoCard.append(inspoAuthorTag)
+    
+                inspoImgTag = document.createElement("img")
+                inspoImgTag.src = inspo.image
+                inspoImgTag.className = "inspo-avatar"
+                inspoCard.append(inspoImgTag)
+    
+                inspoQuoteTag = document.createElement("h4")
+                inspoQuote = document.createTextNode(inspo.quote)
+                inspoQuoteTag.appendChild(inspoQuote)
+                inspoCard.append(inspoQuoteTag)
+    
+                inspoLikesTag = document.createElement("p")
+                inspoLikesTag.className = "inspo-likes"
+                inspoLikesTag.innerText = `${inspo.likes} Likes`
+                inspoCard.append(inspoLikesTag)
+    
+                likesBttnTag = document.createElement("div")
+                likesBttnTag.className = "like-btn"
+                likesBttn = document.createTextNode("ॐ")
+                likesBttnTag.appendChild(likesBttn)
+                inspoCard.append(likesBttnTag)
+                
+    
+    
+                inspoCollection.append(inspoCard);
+                // console.log(inspoCollection);
+            });
+            
         }
       });
 });
